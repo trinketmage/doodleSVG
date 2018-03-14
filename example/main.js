bb30f99c600e103810b97820c55bbd7e41b9ca71
trinktmage <tranremi@live.fr>
2018-03-14 18:52:05 +0100

    update example

--- example/main.js
+++ example/main.js
@@ -1,7 +1,7 @@
 (function() {
     var SPN = new doodleSVG();
 
-    var list = document.querySelectorAll("path, rect, polygon, polyline, line, circle");
+    var list = document.querySelectorAll("path, rect, polygon, polyline, line, circle, ellipse");
     var newObjArray = SPN.normalizeGroup(list);
 
     // var el = document.querySelector("path");
